c9b284b756cce4a05fe9d4b32ea8d45c4829d096
Ruben Kuipers <info@rkmediadesign.nl>
2016-08-23 01:40:15 +0200

    Change app component markup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,20 +1,18 @@
 /* tslint:disable:no-unused-variable */
 
-import { addProviders, async, inject } from '@angular/core/testing';
+import {
+  beforeEach, beforeEachProviders,
+  describe, xdescribe,
+  expect, it, xit,
+  async, inject
+} from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-describe('App: Angular2NoteApp', () => {
-  beforeEach(() => {
-    addProviders([AppComponent]);
-  });
+beforeEachProviders(() => [AppComponent]);
 
+describe('App: Note', () => {
   it('should create the app',
-    inject([AppComponent], (app: AppComponent) => {
-      expect(app).toBeTruthy();
-    }));
-
-  it('should have as title \'app works!\'',
-    inject([AppComponent], (app: AppComponent) => {
-      expect(app.title).toEqual('app works!');
-    }));
+      inject([AppComponent], (app: AppComponent) => {
+    expect(app).toBeTruthy();
+  }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { NoteAppComponent} from './note-app';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [NoteAppComponent]
 })
 export class AppComponent {
-  title = 'app works!';
+
 }
